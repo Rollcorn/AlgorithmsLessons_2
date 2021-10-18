a69335fcafd2953c779b92d6d53868a15c8bf764
rollcorn <lazydaimon@gmail.com>
2021-10-18 20:14:53 +0300

    Update files

---
+++ Lesson2/src/ru/shkandyuk/BST.java
@@ -0,0 +1,78 @@
+package ru.shkandyuk;
+
+import java.io.*;
+import java.util.*;
+
+class BSTNode<T> {
+
+    public int NodeKey; // ключ узла
+
+    public T NodeValue; // значение в узле
+
+    public BSTNode<T> Parent; // родитель или null для корня
+
+    public BSTNode<T> LeftChild; // левый потомок
+
+    public BSTNode<T> RightChild; // правый потомок
+
+    public BSTNode(int key, T val, BSTNode<T> parent) {
+        NodeKey = key;
+        NodeValue = val;
+        Parent = parent;
+        LeftChild = null;
+        RightChild = null;
+    }
+}
+
+// промежуточный результат поиска
+class BSTFind<T> {
+    public BSTNode<T> Node; // null если в дереве вообще нету узлов
+    public boolean NodeHasKey; // true если узел найден
+    public boolean ToLeft; // true, если родительскому узлу надо добавить новый левым
+
+    public BSTFind() { Node = null; }
+}
+
+class BST<T> {
+    BSTNode<T> Root; // корень дерева, или null
+
+    public BST(BSTNode<T> node) {
+        Root = node;
+    }
+
+    /*****************************************************************
+     * Метод поиска узла дерева с определенным ключом
+     * @param key
+     * @return
+     */
+    public BSTFind<T> FindNodeByKey(int key) {
+        // ищем в дереве узел и сопутствующую информацию по ключу
+        return null;
+    }
+
+    /**************************************************************************************
+     * Метод добавления нового узла, задаём добавляемый ключ и соответствующее ему значение
+     * @param key
+     * @param val
+     * @return
+     */
+    public boolean AddKeyValue(int key, T val) {
+        // добавляем ключ-значение в дерево
+        return false; // если ключ уже есть
+    }
+
+    public BSTNode<T> FinMinMax(BSTNode<T> FromNode, boolean FindMax) {
+        // ищем максимальный/минимальный ключ в поддереве
+        return null;
+    }
+
+    public boolean DeleteNodeByKey(int key) {
+        // удаляем узел по ключу
+        return false; // если узел не найден
+    }
+
+    public int Count() {
+        return 0; // количество узлов в дереве
+    }
+
+}
