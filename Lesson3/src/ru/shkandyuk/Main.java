e43f889795a7b9fc26dc003966d2f13aaffebd33
denis shkandyuk <lazydaimon@gmail.com>
2021-10-25 19:15:45 +0300

    Update files

--- Lesson3/src/ru/shkandyuk/Main.java
+++
@@ -1,8 +0,0 @@
-package ru.shkandyuk;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ Lesson3/test/ru.shkandyuk/testArratBST.java
@@ -0,0 +1,18 @@
+package ru.shkandyuk;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class testArratBST {
+
+    @Before
+    public void start(){
+        aBST abst = new aBST(3);
+        System.out.println("Size: " + abst.Tree.length );
+
+    }
+
+    @Test
+    public void testConstructur(){
+    }
+}
