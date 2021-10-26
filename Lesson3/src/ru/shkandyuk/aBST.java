8f5e5996887770bfe3c6c1b16f2dc797159dfeea
denis shkandyuk <lazydaimon@gmail.com>
2021-10-26 18:10:12 +0300

    Upload first commit

--- Lesson3/src/ru/shkandyuk/aBST.java
+++ Lesson3/src/ru/shkandyuk/aBST.java
@@ -1,35 +1,96 @@
-package ru.shkandyuk;
+//package ru.shkandyuk;
 
 import java.util.*;
 
-class aBST
-{
-    public Integer Tree []; // массив ключей
+class aBST {
+    public Integer Tree[]; // массив ключей
 
-    public aBST(int depth)
-    {
+    public aBST(int depth) {
         // правильно рассчитайте размер массива для дерева глубины depth:
         int tree_size = 0;
-        for(int i = 0; i <= depth; i++){
+        for (int i = 0; i <= depth; i++) {
             tree_size += (1 << i);
         }
-        Tree = new Integer[ tree_size ];
-        for( int i = 0; i < tree_size; i++ ) {
+        Tree = new Integer[tree_size];
+        for (int i = 0; i < tree_size; i++) {
             Tree[i] = null;
         }
     }
 
-    public Integer FindKeyIndex(int key)
-    {
+    public Integer FindKeyIndex(int key) {
+        Integer ret = null;
+        Integer curKey = 0;
+
         // ищем в массиве индекс ключа
-        return null; // не найден
+        for ( int index = 0; index < Tree.length && ret == null; ) {
+            curKey = Tree[index];
+            if ( curKey == null || curKey == key ) {
+                ret = index;
+            }
+            else if ( key < curKey  ) {
+                index = getLeftChildIndex(index);
+            }
+            else {
+                index = getRightChildIndex(index);
+            }
+        }
+
+        return ret; // не найден
     }
 
-    public int AddKey(int key)
-    {
+    public int AddKey(int key) {
+        int ret = -1;
+        Integer curKey = 0;
+
         // добавляем ключ в массив
-        return -1;
+        for ( int index = 0; index < Tree.length && ret == -1; ) {
+
+            curKey = Tree[index];
+
+            if ( curKey == null || curKey == key ) {
+                Tree[index] = key;
+                ret = index;
+            }
+            else if ( key < curKey  ) {
+                index = getLeftChildIndex(index);
+            }
+            else {
+                index = getRightChildIndex(index);
+            }
+        }
         // индекс добавленного/существующего ключа или -1 если не удалось
+        return ret;
     }
 
+    private Integer getLeftChildIndex(Integer aIndex) {
+        Integer ret;
+        if ( aIndex == null ) {
+            ret = null;
+        } else {
+            ret = 2 * aIndex + 1;
+        }
+        return ret;
+    }
+
+    private Integer getRightChildIndex(Integer aIndex) {
+        Integer ret;
+        if ( aIndex == null ) {
+            ret = null;
+        } else {
+            ret = 2 * aIndex + 2;
+        }
+        return ret;
+    }
+
+    public void printTree(){
+        int border = 1;
+        for (int i = 0; i != Tree.length; i++) {
+            if (i == ( border - 1 ) )  {
+                System.out.println("----------");
+                border = border << 1;
+            }
+            System.out.println("[" + i + "] = " + Tree[i] );
+
+        }
+    }
 }
--- Lesson3/test/ru.shkandyuk/testArratBST.java
+++ Lesson3/test/ru.shkandyuk/testArratBST.java
@@ -1,18 +1,50 @@
 package ru.shkandyuk;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class testArratBST {
+    aBST abst = new aBST(3);
 
     @Before
-    public void start(){
-        aBST abst = new aBST(3);
+    public void before(){
         System.out.println("Size: " + abst.Tree.length );
+    }
 
+    @After
+    public void after(){
+        abst.printTree();
     }
 
     @Test
-    public void testConstructur(){
+    public void testAddKey(){
+        abst.AddKey(4);
+        abst.AddKey(6);
+        abst.AddKey(2);
+        abst.AddKey(3);
+        abst.AddKey(1);
+        abst.AddKey(5);
+        abst.AddKey(7);
+        abst.AddKey(0);
+        abst.AddKey(9);
+    }
+
+    @Test
+    public void testFindKey(){
+        abst.AddKey(4);
+        abst.AddKey(6);
+        abst.AddKey(2);
+        abst.AddKey(3);
+        abst.AddKey(1);
+        abst.AddKey(5);
+        abst.AddKey(7);
+        abst.AddKey(0);
+        abst.AddKey(9);
+
+        System.out.println("Find index [5]: " + abst.FindKeyIndex(5) );
+        System.out.println("Find index [0]: " + abst.FindKeyIndex(0) );
+        System.out.println("Find index [9]: " + abst.FindKeyIndex(9) );
+
     }
 }
