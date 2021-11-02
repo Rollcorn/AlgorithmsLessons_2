2822308f9e2a0e025d61a1490b3fa47c0fd81c4e
rollcorn <lazydaimon@gmail.com>
2021-11-02 15:18:55 +0300

    Create method GenerateBBSTArray

--- Lesson4/src/ru/shkandyuk/AlgorithmsDataStructures2.java
+++ Lesson4/src/ru/shkandyuk/AlgorithmsDataStructures2.java
@@ -1,14 +1,45 @@
-package ru.shkandyuk;
+//package ru.shkandyuk;
 
 import java.util.*;
 
 public class AlgorithmsDataStructures2 {
 
-    public static int[] GenerateBBSTArray(int[] a)
-    {
-        int[] res = a.clone();
-        Arrays.sort(res);
+    public static int[] GenerateBBSTArray(int[] a) {
 
-        return res;
+        if (a == null || a.length <= 1) {
+            return a;
+        }
+
+        // Сортируем массив
+        int[] sortArr = a;
+        Arrays.sort(sortArr);
+
+        int begin = 0;
+        int end = sortArr.length; // ind of obj outside the array
+        int middle = end / 2;
+
+        int[] bstArr = new int[a.length];
+        bstArr[0] = sortArr[middle];
+        int[] leftArr = Arrays.copyOfRange(sortArr, begin, middle);
+        int[] rightArr = Arrays.copyOfRange(sortArr, (middle + 1), end);
+        int[] leftTree = GenerateBBSTArray(leftArr);
+        int[] rightTree = GenerateBBSTArray(rightArr);
+
+        int i = 1;
+        int l = 0;
+        int r = 0;
+        int part = 1;
+        while ( i < bstArr.length ) {
+            for( int j = 0; j < part && l < leftTree.length; j++, i++, l++ ){
+                bstArr[i] = leftTree[l];
+            }
+
+            for( int j = 0; j < part && r < rightTree.length; j++, i++, r++ ){
+                bstArr[i] = rightTree[r];
+            }
+            part = part << 1;
+        }
+        return bstArr;
     }
+
 }
--- Lesson4/test/ru/shkandyuk/testAlgorithmsDataStructures2.java
+++ Lesson4/test/ru/shkandyuk/testAlgorithmsDataStructures2.java
@@ -4,21 +4,50 @@ import org.junit.Test;
 
 public class testAlgorithmsDataStructures2 {
 
-    int[] a = { 4, 9, 8, 3, 1, 7, 6, 2, 5};
+    /****************************************************************
+     * sort =
+     *                      { 1 2 3 4 5 6 7 8 9}:9
+     *                      /         |         \
+     *             {1 2 3 4 }:4       5        { 6 7 8 9 }:4
+     *             /    |    \                 /     |   \
+     *      { 1 2 }:2   3    { 4 }:1    { 6 7 }:2    8    { 9 }:1
+     *      /   |              |        /   |               |
+     *   { 1 }  2              4     { 6 }  7               9
+     *
+     *   5 3 2 1 4 8 7 6 9
+     *   5 3 8 2 4 7 9 1 x x x 6 x x x
+     *   H = 1 +
+     *
+     *                                 ___ 5 ___
+     *                                /          \
+     *                              3              8
+     *                            /   \          /  \
+     *                           2      4       7    9
+     *                          /  \   / \     / \  / \
+     *                         1              6
+     */
+
+    int[] a = { 11, 4, 9, 8, 14, 3, 12, 15, 1, 7, 6, 2, 5, 10, 13};
 
     AlgorithmsDataStructures2 obj = new AlgorithmsDataStructures2();
 
     @Test
     public void testGenerateBbstArray(){
         int[] sortArr = AlgorithmsDataStructures2.GenerateBBSTArray(a);
-
-        for (int x: sortArr) {
-            System.out.println(x);
-        }
-
+        System.out.print("Изначальный массив [ ");
         for (int x: a) {
-            System.out.println(x);
+            System.out.print(x + " ");
         }
+        System.out.println("]");
+
+        System.out.print("Массив дерево: [ ");
+        for (int x: sortArr) {
+            System.out.print(x + " ");
+        }
+        System.out.println("]");
+
+
+
     }
 
 
