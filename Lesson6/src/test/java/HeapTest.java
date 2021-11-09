479c2359328bde3dd14670d2489a25dd29c07f4e
denis shkandyuk <lazydaimon@gmail.com>
2021-11-09 19:18:51 +0300

    Create Heap.class project

---
+++ Lesson6/src/test/java/HeapTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeapTest {
+
+    @Test
+    void makeHeap() {
+    }
+
+    @Test
+    void getMax() {
+    }
+
+    @Test
+    void add() {
+    }
+}
