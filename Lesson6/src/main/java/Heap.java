fa6c06dca8627d3e4e123163c8d6566d315bee32
Rollcorn <57489789+Rollcorn@users.noreply.github.com>
2021-11-16 13:12:25 +0300

    Delete comments

--- Lesson6/src/main/java/Heap.java
+++ Lesson6/src/main/java/Heap.java
@@ -1,7 +1,7 @@
 //import java.util.*;
 
 class Heap {
-    public int[] HeapArray; // хранит неотрицательные числа-ключи
+    public int[] HeapArray;
     int size = 0;
 
     public Heap() {
@@ -9,15 +9,11 @@ class Heap {
     }
 
     public void MakeHeap(int[] a, int depth) {
-        // Расчет необходимой длины массива для размещения кучи заданной глубины
         int arrLength = 0;
-        // размер массива кучи вычисляется на основе глубины кучи
         for (int i = 0; i < depth; i++) {
             arrLength = arrLength + (1 << i);
         }
-        // Инициализация количества эллементов в массиве
         HeapArray = new int[arrLength];
-        // создаём массив кучи HeapArray из заданного
         for (int j : a) {
             Add(j);
         }
@@ -56,24 +52,15 @@ class Heap {
 
 
         }
-        // вернуть значение корня и перестроить кучу
-        return max; // если куча пуста
+        return max;
     }
 
-    /************************************
-     * Добавление новго элемента в кучу
-     * @param key
-     * @return
-     */
     public boolean Add(int key) {
-        // проверить что в массиве есть место
         if ( HeapArray == null || size >= HeapArray.length) {
             return false;
         }
-        // поместить новый элемент после последнего элемента
         HeapArray[size] = key;
 
-        // пока родительский элемент меньше нового элемента просеиваем новый элемент вверх кучи
         for (int i = size; ( Parent(i) >= 0 ) && ( HeapArray[i] > HeapArray[Parent(i)] ); i = Parent(i) ) {
             int tmp = HeapArray[Parent(i)];
             HeapArray[Parent(i)] = HeapArray[i];
@@ -81,20 +68,17 @@ class Heap {
 
         }
         size++;
-        return true; // если куча вся заполнена
+        return true; 
     }
 
-    // Индекс родительского элемента
     public int Parent(int i) {
         return i / 2;
     }
 
-    // Индекс левого дочернего элемента
     public int Left(int i) {
         return 2 * i + 1;
     }
 
-    // Индекс правого дочернего элемента
     public int Right(int i) {
         return 2 * i + 2;
     }
