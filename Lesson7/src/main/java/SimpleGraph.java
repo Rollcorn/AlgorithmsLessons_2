1fc1b831950d285068e6c6160c72107f5f9ede90
rollcorn <lazydaimon@gmail.com>
2021-11-20 19:27:53 +0300

    Graph realization class'

--- Lesson7/src/main/java/SimpleGraph.java
+++ Lesson7/src/main/java/SimpleGraph.java
@@ -11,44 +11,103 @@ class Vertex
 
 class SimpleGraph
 {
-    Vertex [] vertex;
-    int [][] m_adjacency;
-    int max_vertex;
+    Vertex [] vertex;       // список vertex, хранящий вершины
+    int [][] m_adjacency;   // матрица смежности
+    int max_vertex;         // максимальное количество вершин
 
     public SimpleGraph(int size)
     {
         max_vertex = size;
         m_adjacency = new int [size][size];
         vertex = new Vertex[size];
+
+        Arrays.fill(vertex, null);
+
+        for ( int i = 0; i != m_adjacency.length; i++ )
+        {
+            for ( int j = 0; j != m_adjacency.length; j++ )
+            {
+                m_adjacency[i][j] = 0;
+            }
+        }
     }
 
     public void AddVertex(int value)
     {
-        // ваш код добавления новой вершины
+        // добавления новой вершины
         // с значением value
         // в незанятую позицию vertex
+        for ( int i = 0; i < vertex.length; i++ ) {
+            if ( vertex[i] == null) {
+                vertex[i] = new Vertex(value);
+                return;
+            }
+        }
+
     }
 
     // здесь и далее, параметры v -- индекс вершины
     // в списке  vertex
     public void RemoveVertex(int v)
     {
+        if ( v < 0 || v >= vertex.length ) {
+            return;
+        }
         // ваш код удаления вершины со всеми её рёбрами
+        for ( int i = 0; i < vertex.length; i++ ) {
+            if ( m_adjacency[v][i] == 1 ) {
+                m_adjacency[i][v] = 0;
+                m_adjacency[v][i] = 0;
+            }
+        }
+        vertex[v] = null;
+
     }
 
+    // true если есть ребро между вершинами v1 и v2
     public boolean IsEdge(int v1, int v2)
     {
-        // true если есть ребро между вершинами v1 и v2
-        return false;
+        boolean res;
+        if ( (v1 < 0) || (v2 < 0) || (v1 >= vertex.length) || (v2 >= vertex.length) )
+        {
+            res =  false;
+        } else {
+            res = (m_adjacency[v1][v2] == 1) && (m_adjacency[v2][v1] == 1);
+        }
+        return res;
     }
 
     public void AddEdge(int v1, int v2)
     {
+        if ( !( (v1 < 0) || (v2 < 0) || (v1 >= vertex.length) || (v2 >= vertex.length) ) )
+        {
+            m_adjacency[v1][v2] = 1;
+            m_adjacency[v2][v1] = 1;
+        }
         // добавление ребра между вершинами v1 и v2
     }
 
+    // удаление ребра между вершинами v1 и v2
     public void RemoveEdge(int v1, int v2)
     {
-        // удаление ребра между вершинами v1 и v2
+        if ( !( (v1 < 0) || (v2 < 0) || (v1 >= vertex.length) || (v2 >= vertex.length) ) )
+        {
+            m_adjacency[v1][v2] = 0;
+            m_adjacency[v2][v1] = 0;
+        }
+
+    }
+
+    public int vertexSize()
+    {
+        int size = 0;
+        for (Vertex i: vertex)
+        {
+            if (i != null)
+            {
+                size++;
+            }
+        }
+        return size;
     }
 }
--- Lesson7/src/test/java/SimpleGraphTest.java
+++ Lesson7/src/test/java/SimpleGraphTest.java
@@ -4,28 +4,136 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleGraphTest {
+    SimpleGraph g1 = new SimpleGraph(10);
 
     @BeforeEach
     void setUp() {
+        g1.AddVertex(1);
+        g1.AddVertex(2);
+        g1.AddVertex(3);
+        g1.AddVertex(4);
+        g1.AddVertex(5);
+        g1.AddVertex(6);
+        g1.AddVertex(7);
+        g1.AddVertex(8);
+        g1.AddVertex(9);
+        g1.AddVertex(10);
     }
 
     @Test
     void addVertex() {
+        System.out.println("Vertex size: " + g1.vertexSize());
+        System.out.print("[ ");
+        for (Vertex i: g1.vertex) {
+            if ( i != null ) {
+                System.out.print(i.Value + ", ");
+            } else {
+                System.out.print("null, ");
+            }
+        }
+        System.out.println(" ]");
     }
 
     @Test
     void removeVertex() {
+        printGraph();
+        System.out.println("1: Vertex size: " + g1.vertexSize());
+        g1.AddEdge(0, 5);
+        g1.AddEdge(1, 2);
+        g1.AddEdge(3, 3);
+        g1.AddEdge(7, 8);
+        g1.AddEdge(9, 3);
+        g1.AddEdge(10,10);
+        g1.AddEdge(10,10);
+        printGraph();
+        System.out.println("2: Vertex size: " + g1.vertexSize());
+        g1.RemoveVertex(3);
+        printGraph();
+        System.out.println("3: Vertex size: " + g1.vertexSize());
+        g1.RemoveVertex(10);
+        printGraph();
+        System.out.println("4: Vertex size: " + g1.vertexSize());
+        g1.RemoveVertex(3);
+        printGraph();
+        System.out.println("5: Vertex size: " + g1.vertexSize());
+
     }
 
     @Test
     void isEdge() {
+        assertFalse(g1.IsEdge(0, 5));
+        g1.AddEdge(0, 5);
+        assertTrue(g1.IsEdge(0, 5));
+
+        assertFalse(g1.IsEdge(1, 2));
+        g1.AddEdge(1, 2);
+        assertTrue(g1.IsEdge(1, 2));
+
+        assertFalse(g1.IsEdge(3, 3));
+        g1.AddEdge(3, 3);
+        assertTrue(g1.IsEdge(3, 3));
+
+        assertFalse(g1.IsEdge(7, 8));
+        g1.AddEdge(7, 8);
+        assertTrue(g1.IsEdge(7, 8));
+
+        assertFalse(g1.IsEdge(7, 8));
+        g1.AddEdge(9, 3);
+        assertTrue(g1.IsEdge(7, 8));
+
     }
 
     @Test
     void addEdge() {
+        System.out.println("Vertex size: " + g1.vertexSize());
+        g1.AddEdge(0, 5);
+        g1.AddEdge(1, 2);
+        g1.AddEdge(3, 3);
+        g1.AddEdge(7, 8);
+        g1.AddEdge(9, 3);
+        g1.AddEdge(10,10);
+
+
+
     }
 
     @Test
     void removeEdge() {
+        printGraph();
+        g1.AddEdge(0, 5);
+        g1.AddEdge(1, 2);
+        g1.AddEdge(3, 3);
+        g1.AddEdge(7, 8);
+        g1.AddEdge(9, 3);
+        g1.AddEdge(10,10);
+        g1.AddEdge(10,10);
+        printGraph();
+
+        System.out.println("Удаление ребер 1-2 и 9-3");
+        g1.RemoveEdge(1, 2);
+//        g1.RemoveEdge(9, 3);
+        g1.RemoveEdge(5, 5);
+        g1.RemoveEdge(3, 3);
+
+        printGraph();
+
+    }
+
+    public void printGraph(){
+        System.out.print( "     ");
+        for ( int j = 0; j != g1.m_adjacency.length; j++ )
+        {
+            System.out.print( j + "  " );
+        }
+        System.out.println(" ");
+        for ( int i = 0; i != g1.m_adjacency.length; i++ )
+        {
+            System.out.print( i + ": [ ");
+            for ( int j = 0; j != g1.m_adjacency.length; j++ )
+            {
+                System.out.print( g1.m_adjacency[i][j] + ", " );
+            }
+            System.out.println("]");
+        }
     }
 }
